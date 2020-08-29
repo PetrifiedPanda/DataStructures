90ba2bc7892021e99a8d0b365fb4a5f6e378723a
PetrifiedPanda <bfj.koehn@gmail.com>
2020-08-29 17:20:45 +0200

    Added some range checks to TreeNodeIt

--- BinarySearchTree/TreeNodeIt.h
+++ BinarySearchTree/TreeNodeIt.h
@@ -42,51 +42,71 @@ bool TreeNodeIt<T>::isValid() const {
 
 template <typename T>
 bool TreeNodeIt<T>::hasLeftChild() const {
+    if (!isValid())
+        throw std::runtime_error("Tried to check left child of null node");
     return currentNode_->left != nullptr;
 }
 
 template <typename T>
 bool TreeNodeIt<T>::hasRightChild() const {
+    if (!isValid())
+        throw std::runtime_error("Tried to check right child of null node");
     return currentNode_->right != nullptr;
 }
 
 template <typename T>
 bool TreeNodeIt<T>::hasParent() const {
+    if (!isValid())
+        throw std::runtime_error("Tried to check parent of null node");
     return currentNode_->parent != nullptr;
 }
 
 template <typename T>
 const T& TreeNodeIt<T>::key() const {
+    if (!isValid())
+        throw std::runtime_error("Tried to get key of null node");
     return currentNode_->key;
 }
 
 template <typename T>
 TreeNodeIt<T>& TreeNodeIt<T>::left() {
+    if (!isValid())
+        throw std::runtime_error("Tried to move to left node of null node");
     currentNode_ = currentNode_->left.get();
 }
 
 template <typename T>
 TreeNodeIt<T> TreeNodeIt<T>::getLeft() const {
+    if (!isValid())
+        throw std::runtime_error("Tried to get left child of null node");
     return TreeNodeIt(currentNode_->left.get());
 }
 
 template <typename T>
 TreeNodeIt<T>& TreeNodeIt<T>::right() {
+    if (!isValid())
+        throw std::runtime_error("Tried to move to right node of null node");
     currentNode_ = currentNode_->right.get();
 }
 
 template <typename T>
 TreeNodeIt<T> TreeNodeIt<T>::getRight() const {
+    if (!isValid())
+        throw std::runtime_error("Tried to get right child of null node");
     return TreeNodeIt(currentNode_->right.get());
 }
 
 template <typename T>
 TreeNodeIt<T>& TreeNodeIt<T>::parent() {
+    if (!isValid())
+        throw std::runtime_error("Tried to move to parent of null node");
     currentNode_ = currentNode_->parent;
 }
 
 template <typename T>
 TreeNodeIt<T> TreeNodeIt<T>::getParent() const {
+    if (!isValid())
+        throw std::runtime_error("Tried to get parent of null node");
     return TreeNodeIt(currentNode_->parent);
 }
 
