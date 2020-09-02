f25ae901a5a133b864caabdaff46ed5051d3e145
PetrifiedPanda <bfj.koehn@gmail.com>
2020-09-02 13:12:32 +0200

    Made constructor pass by const reference and removed unised constructor

--- BinarySearchTree/TreeNode.h
+++ BinarySearchTree/TreeNode.h
@@ -10,6 +10,5 @@ class TreeNode {
     std::unique_ptr<TreeNode> right;
     TreeNode<T>* parent;
 
-    TreeNode(T key) : key(key), left(nullptr), right(nullptr), parent(nullptr) {}
-    TreeNode(T key, TreeNode<T>* p) : key(key), parent(p) {}
+    TreeNode(const T& key) : key(key), left(nullptr), right(nullptr), parent(nullptr) {}
 };
