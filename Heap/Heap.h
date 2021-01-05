c1c8a7c723604f75a16b1755a0722bc65652afc9
PetrifiedPanda <bfj.koehn@gmail.com>
2021-01-05 17:16:29 +0100

    Made Heap use a constant template comparator that is set to std::less by default. This required removal of assignment operators and the setComparator() method

--- Heap/Heap.h
+++ Heap/Heap.h
@@ -4,24 +4,21 @@
 #include <functional>
 #include <vector>
 
-template <typename T,
+template <typename T, 
+          class Comp = std::less<T>,
           class Container = std::vector<T>>
 class Heap {
     Container data_;
-    std::function<bool(const T&, const T&)> comparator_;
+    const Comp comparator_;
 
    public:
-    explicit Heap(const std::function<bool(const T&, const T&)>& comp) : comparator_(comp) {}
-    Heap(std::initializer_list<T> init, const std::function<bool(const T&, const T&)>& comp);
+    explicit Heap(const Comp& comp = Comp()) : comparator_(comp) {}
+    Heap(std::initializer_list<T> init, const Comp& comp = Comp());
 
-    Heap(Heap<T, Container>& heap) : data_(heap.data_), comparator_(heap.comparator_) {}
-    Heap(Heap<T, Container>&& heap) noexcept : data_(std::move(heap.data_)), comparator_(std::move(heap.comparator_)) {}
+    Heap(Heap<T, Comp, Container>& heap) : data_(heap.data_), comparator_(heap.comparator_) {}
+    Heap(Heap<T, Comp, Container>&& heap) noexcept : data_(std::move(heap.data_)), comparator_(std::move(heap.comparator_)) {}
 
-    Heap<T, Container>& operator=(const Heap<T, Container>& heap);
-    Heap<T, Container>& operator=(Heap<T, Container>&& heap);
-    Heap<T, Container>& operator=(std::initializer_list<T> init);
-
-    void setComparator(const std::function<bool(const T&, const T&)>& comp);
+    Heap<T, Comp, Container>& operator=(std::initializer_list<T> init);
 
     void insert(const T& key);
 
@@ -33,7 +30,6 @@ class Heap {
 
    private:
     void downHeapify(size_t startIndex);
-    void biDirHeapify(size_t startIndex);
 
     void buildHeap();
 
@@ -46,45 +42,25 @@ class Heap {
 
 // Constructors
 
-template <typename T, class Container>
-Heap<T, Container>::Heap(std::initializer_list<T> init, const std::function<bool(const T&, const T&)>& comp) : Heap(comp) {
+template <typename T, class Comp, class Container>
+Heap<T, Comp, Container>::Heap(std::initializer_list<T> init, const Comp& comp) : Heap(comp) {
     data_ = init;
     buildHeap();
 }
 
 // Equality operators
 
-template <typename T, class Container>
-Heap<T, Container>& Heap<T, Container>::operator=(const Heap<T, Container>& heap) {
-    data_ = heap.data_;
-    comparator_ = heap.comparator_;
-    return *this;
-}
-
-template <typename T, class Container>
-Heap<T, Container>& Heap<T, Container>::operator=(Heap<T, Container>&& heap) {
-    data_ = std::move(heap.data_);
-    comparator_ = std::move(heap.comparator_);
-    return *this;
-}
-
-template <typename T, class Container>
-Heap<T, Container>& Heap<T, Container>::operator=(std::initializer_list<T> init) {
+template <typename T, class Comp, class Container>
+Heap<T, Comp, Container>& Heap<T, Comp, Container>::operator=(std::initializer_list<T> init) {
     data_ = init;
     buildHeap();
     return *this;
 }
 
-template <typename T, class Container>
-void Heap<T, Container>::setComparator(const std::function<bool(const T&, const T&)>& comp) {
-    comparator_ = comp;
-    buildHeap();
-}
-
 // Insert operation
 
-template <typename T, class Container>
-void Heap<T, Container>::insert(const T& key) {
+template <typename T, class Comp, class Container>
+void Heap<T, Comp, Container>::insert(const T& key) {
     data_.push_back(key);
 
     size_t i = data_.size() - 1;
@@ -96,8 +72,8 @@ void Heap<T, Container>::insert(const T& key) {
 
 // Delete operations
 
-template <typename T, class Container>
-T Heap<T, Container>::extractExtremum() {
+template <typename T, class Comp, class Container>
+T Heap<T, Comp, Container>::extractExtremum() {
     T max = data_[0];
     data_[0] = data_[data_.size() - 1];
     data_.erase(--data_.end());
@@ -105,20 +81,20 @@ T Heap<T, Container>::extractExtremum() {
     return max;
 }
 
-template <typename T, class Container>
-void Heap<T, Container>::clear() {
+template <typename T, class Comp, class Container>
+void Heap<T, Comp, Container>::clear() {
     data_.clear();
 }
 
-template <typename T, class Container>
-size_t Heap<T, Container>::size() {
+template <typename T, class Comp, class Container>
+size_t Heap<T, Comp, Container>::size() {
     return data_.size();
 }
 
 // Utility functions
 
-template <typename T, class Container>
-void Heap<T, Container>::downHeapify(size_t startIndex) {
+template <typename T, class Comp, class Container>
+void Heap<T, Comp, Container>::downHeapify(size_t startIndex) {
     int maxIndex = startIndex;
     int prevIndex = -1;
     while (maxIndex != prevIndex) {
@@ -135,52 +111,30 @@ void Heap<T, Container>::downHeapify(size_t startIndex) {
     }
 }
 
-template <typename T, class Container>
-void Heap<T, Container>::biDirHeapify(size_t startIndex) {
-    int maxIndex = startIndex;
-    int prevIndex = -1;
-    while (maxIndex != prevIndex) {
-        prevIndex = maxIndex;
-
-        if (parent(prevIndex) >= 0 && parent(prevIndex) < data_.size() && comparator_(data_[maxIndex], data_[parent(prevIndex)]))
-            maxIndex = parent(prevIndex);
-        else {
-            if (left(prevIndex) < data_.size() && !comparator_(data_[maxIndex], data_[left(prevIndex)]))
-                maxIndex = left(prevIndex);
-
-            if (right(prevIndex) < data_.size() && !comparator_(data_[maxIndex], data_[right(prevIndex)]))
-                maxIndex = right(prevIndex);
-        }
-
-        if (maxIndex != prevIndex)
-            swap(prevIndex, maxIndex);
-    }
-}
-
-template <typename T, class Container>
-void Heap<T, Container>::buildHeap() {
+template <typename T, class Comp, class Container>
+void Heap<T, Comp, Container>::buildHeap() {
     int size = data_.size();
     for (int i = ceil((size - 1) / 2.0) - 1; i > -1; --i)
         downHeapify(i);
 }
 
-template <typename T, class Container>
-size_t Heap<T, Container>::left(size_t index) {
+template <typename T, class Comp, class Container>
+size_t Heap<T, Comp, Container>::left(size_t index) {
     return 2 * (index + 1) - 1;
 }
 
-template <typename T, class Container>
-size_t Heap<T, Container>::right(size_t index) {
+template <typename T, class Comp, class Container>
+size_t Heap<T, Comp, Container>::right(size_t index) {
     return 2 * (index + 1);
 }
 
-template <typename T, class Container>
-size_t Heap<T, Container>::parent(size_t index) {
+template <typename T, class Comp, class Container>
+size_t Heap<T, Comp, Container>::parent(size_t index) {
     return ceil(index / 2.0) - 1;
 }
 
-template <typename T, class Container>
-void Heap<T, Container>::swap(size_t i1, size_t i2) {
+template <typename T, class Comp, class Container>
+void Heap<T, Comp, Container>::swap(size_t i1, size_t i2) {
     T tmp = std::move(data_[i1]);
     data_[i1] = std::move(data_[i2]);
     data_[i2] = std::move(tmp);
--- test/HeapTest.cpp
+++ test/HeapTest.cpp
@@ -3,9 +3,9 @@
 #include "Heap/Heap.h"
 
 struct HeapTests : public testing::Test {
-    Heap<int> heap = Heap<int>([](const int& x1, const int& x2) { return x1 < x2; });
+    Heap<int> heap = Heap<int>();
     virtual void SetUp() override {
-        heap = Heap<int>([](const int& x1, const int& x2) { return x1 < x2; });
+        
     }
 
     virtual void TearDown() override {
@@ -14,20 +14,14 @@ struct HeapTests : public testing::Test {
 
 TEST_F(HeapTests, BasicUsage) {
     // Don't use predeclared Stuff
-    Heap<double> myHeap([](const int& x1, const int& x2) { return x1 > x2; });
+    Heap<double, std::greater<double>> myHeap = Heap<double, std::greater<double>>();
     myHeap.insert(5.1);
     myHeap.insert(12.3);
     myHeap.insert(3.3);
 
-    Heap<double> copy(myHeap);
+    Heap<double, std::greater<double>> copy(myHeap);
 
     EXPECT_EQ(myHeap.extractExtremum(), 12.3);
     EXPECT_EQ(myHeap.extractExtremum(), 5.1);
     EXPECT_EQ(myHeap.extractExtremum(), 3.3);
-
-    copy.setComparator([](const double& x1, const double& x2) { return x1 < x2; });
-
-    EXPECT_EQ(copy.extractExtremum(), 3.3);
-    EXPECT_EQ(copy.extractExtremum(), 5.1);
-    EXPECT_EQ(copy.extractExtremum(), 12.3);
 }
